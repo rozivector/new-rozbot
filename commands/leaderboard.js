9430c689f97594b04c3be76665d6e032ec497788
Rozi Vector <rozi.qood16@gmail.com>
2019-02-03 17:47:37 +0700

    Update leaderboard.js
    Content update may happened in this command

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -4,7 +4,7 @@ exports.run = async(client, msg, args) => {
 
 msg.channel.send("<:rozivector:467815732974059530> **ROZI'S FRIENDS LEADERBOARD:** \n\n**Superstar Hero:**\n1. Rozi Vector#6255 \n2. Noob Slayer#1166 \n3. Faiq#8455 \n4. Exeller Exetior#2799 \n5. Angry Lobster#2873 \n\n**Veteran:**\n1. DarkShadow7754#6153 \n2. SashBrony OC#4637 \n\n**Rookie:**\n1. Meme Justice#6862 \n2. Cloverleaf Pallete#7541 \n3. Storm the Hedgehog#4898 \n4. CruzerBlade Aviation#7700 \n5. Ghostery#0267");
 
-}
+} //this content can be changed periodically
 
 module.exports.help = {
   name: "leaderboard"
